dd66c9a77773f74e09214b41eff0784829f9a2d9
tomosn <flat.earth.lab@gmail.com>
2018-02-03 15:42:25 +0900

    package.json, etc.

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      files: [
+        'src/rasterproj-common.js',
+        'src/rasterproj-aeqd.js',
+        'src/rasterproj-laea.js',
+        'src/rasterproj-tmerc.js',
+        'src/proj-map.js',
+        'src/graticule-renderer.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    uglify: { // task
+      dist: { // target
+        options: {
+          mangle: true,
+          compress: {
+            drop_console: true
+          },
+          banner: [
+            '/*!',
+            ' * <%= pkg.name %> <%= pkg.version %>  <%= grunt.template.today("yyyy-mm-dd") %>',
+            ' *   https://github.com/tomosn/raster-map-projection',
+            ' * Copyright (C) 2016-2018 <%= pkg.author %>',
+            ' * All rights reserved. ',
+            ' * @license GPL v3 License (http://www.gnu.org/licenses/gpl.html)',
+            ' */',
+            ''
+          ].join('\n')
+        },
+
+        files: {
+          'dist/rasterproj-common.min.js': 'src/rasterproj-common.js',
+          'dist/rasterproj-aeqd.min.js': 'src/rasterproj-aeqd.js',
+          'dist/rasterproj-laea.min.js': 'src/rasterproj-laea.js',
+          'dist/rasterproj-tmerc.min.js': 'src/rasterproj-tmerc.js',
+          'dist/proj-map.min.js': 'src/proj-map.js',
+          'dist/graticule-renderer.min.js': 'src/graticule-renderer.js',
+        }
+      }
+    }
+  });
+
+  //  Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  //  default task
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
